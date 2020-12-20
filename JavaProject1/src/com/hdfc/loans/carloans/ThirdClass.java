bb09690db45e950dc99152adc2729680af198ef2
krishnavenirallabandi <rallabandi.krishnaveni28@gmail.com>
2020-12-20 08:15:02 -0800

    added second change in ThirdClass.

--- JavaProject1/src/com/hdfc/loans/carloans/ThirdClass.java
+++ JavaProject1/src/com/hdfc/loans/carloans/ThirdClass.java
@@ -6,7 +6,7 @@ public class ThirdClass {
 		
 		System.out.println("this is third class");
 		System.out.println("this is first change in ThirdClass");
-
+		System.out.println("this is second change in ThirdClass");
 	}
 
 }
